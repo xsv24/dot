cf0ff2ff9196e087278ac4afc4c6c75405876276
Thomas Pearson <thomaspearson.dev@gmail.com>
2020-11-17 12:33:02 +0000

    Add sudo to aliases.

--- vars.zsh
+++ vars.zsh
@@ -8,9 +8,10 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PATH="$(yarn global bin):$PATH" 
 
 # Aliases
-alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
+alias = "sudo " # Allows aliases to work with sudo.
 alias vim="nvim"
 alias vi="nvim"
 alias oldvim="vim"
 
-alias git-tag="git tag -l --sort version:refname | tail -n 1"
+alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
+alias git-tag="git tag -l --sort version:refname | tail -n 10"
