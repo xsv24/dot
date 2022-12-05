0fb16cc71ceafa02423ca345492a9ca684d102e3
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-12-05 21:57:25 +1300

    🐛 Fix up install script and shellcheck scripts.

--- main.sh
+++
@@ -1,5 +0,0 @@
-eval "$(starship init bash)"
-
-source ~/.dot/vars.sh
-source ~/.dot/funcs.sh
-source ~/.dot/plugins/fzf.sh
--- zshrc.zsh
+++ zshrc.zsh
@@ -1,19 +1,15 @@
-# Libs
-source $HOME/.dot/vars.zsh
-source $HOME/.dot/funcs.sh
-
-# Plugins 
-source $HOME/.dot/plugins/fzf.zsh 
-source $HOME/.dot/plugins/kubectl.sh
-
-source $HOME/.oh-my-zsh/oh-my-zsh.sh
-export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters
-# source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source $HOME/fzf-tab-completion/zsh/fzf-zsh-completion.sh
-
-# Scripts
 # Starship https://starship.rs
 eval "$(starship init zsh)"
+
+# Libs
+source "$HOME/.dot/vars.zsh"
+source "$HOME/.dot/funcs.sh"
+
+# ZSH setup
+source "$HOME/.oh-my-zsh/oh-my-zsh.sh"
+export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters
+# source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "$HOME/fzf-tab-completion/zsh/fzf-zsh-completion.sh"
 
 # Increase file limit open
 ulimit -S -n 30000 
@@ -21,3 +17,7 @@ ulimit -S -n 30000
 # Set the zsh into vim mode.
 # bindkey -v
 
+# Plugins
+source "$HOME/.dot/plugins/kubectl.sh"
+# fzf terminal bindings must remain at the end of the file.
+source "$HOME/.dot/plugins/fzf.zsh"
