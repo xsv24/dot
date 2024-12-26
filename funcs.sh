df0f5bf8f7d802f3ad13cce60e2bf26b063a90ec
Thomas Pearson <thomaspearson.dev@gmail.com>
2024-12-26 23:52:01 +1300

    feat(funcs): Add some git helpers

--- funcs.sh
+++ funcs.sh
@@ -47,6 +47,14 @@ function fif {
   rg --files-with-matches --no-messages "$1" | fzf "$FZF_PREVIEW_WINDOW" --preview "rg --ignore-case --pretty --context 10 '$1' {}"
 }
 
+function dif {
+  one=$(mktemp)
+  two=$(mktemp)
+  echo "$1" >"$one"
+  echo "$2" >"$two"
+  git diff --word-diff --word-diff-regex=. --no-index "$two" "$one"
+  rm -f "$one" "$two"
+}
 
 function git_diff {
   local preview
@@ -65,10 +73,64 @@ function git_add {
   echo "$files" | xargs -I{} git add {} && git status --short && return
 }
 
+# TODO: Consider about moving these to git aliases instead
+function git_branch_curr {
+  git rev-parse --abbrev-ref HEAD
+}
+
+function git_branch {
+  branch="$(git for-each-ref refs/heads/ --format='%(refname:short)' | fzf)"
+  echo "$branch"
+}
+
+function git_stash {
+  stash="$(git stash list | fzf)"
+  echo "$stash"
+}
+
+function get_between_braces {
+  echo "$1" | cut -d "{" -f2 | cut -d "}" -f1
+}
+
+function git_stash_apply {
+  stash="$(git_stash)"
+  pos="$(get_between_braces stash)"
+  git stash apply "$pos"
+}
+
+function git_checkout {
+  git checkout "$(git_branch)"
+}
+
 function base64_decode {
   echo "$1" | base64 -d
 }
 
 function base64_encode {
   echo "$1" | base64
+}
+
+# Since commits are signed Github do not allow 'rebase' merges (ノಠ益ಠ)ノ彡┻━┻
+git_rebase_merge() {
+  curr_branch="$(git rev-parse --abbrev-ref HEAD)"
+  pr_branch="${1-$curr_branch}"
+  target_branch="${2-master}"
+
+  if [ -z "$pr_branch" ]; then
+    echo "> PR branch '${pr_branch}' is invalid! _(:3」∠)_"
+    return
+  fi
+
+  echo "Merge '${pr_branch}' ➜ '${target_branch}'"
+  echo -n "? Confirm (y/n): "
+  read -r choice
+  echo
+
+  if [[ "{$choice}" =~ [Yy] ]]; then
+    git checkout "${target_branch}"
+    git merge --ff-only "${pr_branch}"
+    git push -u origin "${target_branch}"
+  else
+    echo "> No worries, see you next time! (๑•̀ㅂ•́)ง✧"
+  fi
 }
