e687ec10f19a7eecba09e9b15204eb10e2b3e977
Thomas Pearson <thomaspearson.dev@gmail.com>
2025-06-21 20:45:33 +1200

    feat(lazyvim): use for mini-files for file tree

---
+++ plugins/lazyvim/lua/plugins/mini-files.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "echasnovski/mini.files",
+    lazy = false,
+    opts = {
+      -- You can configure options here if needed
+      use_as_default_explorer = true,
+    },
+    config = function(_, opts)
+      -- Disable default netrw tree
+      vim.g.loaded_netrwPlugin = 1
+      vim.g.loaded_netrw = 1
+
+      vim.keymap.set("n", "<leader>e", function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end, { desc = "Open mini.files (Directory of Current File)" })
+
+      vim.keymap.set("n", "<leader>E", function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end, { desc = "Open mini.files (Directory of Current File)" })
+
+      -- Auto Load mini-files on start up
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          local arg = vim.fn.argv(0)
+          if arg and vim.fn.isdirectory(arg) == 1 then
+            require("mini.files").open(arg, true)
+          end
+        end,
+      })
+    end,
+  },
+}
---
+++ plugins/lazyvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = false,
+      },
+    },
+    enabled = false,
+  },
+}
