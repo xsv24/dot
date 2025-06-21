eb2d187abfe687a523a9203eba57aa4886366abd
Thomas Pearson <thomaspearson.dev@gmail.com>
2025-06-21 21:02:06 +1200

    feat(lazyvim): update keymaps

--- plugins/lazyvim/lua/config/keymaps.lua
+++ plugins/lazyvim/lua/config/keymaps.lua
@@ -2,30 +2,28 @@
 -- https://github.com/LazyVim/LazyVim/blob/63150fa4c5ec8a6f5c56e9035599a8c8e32dc8ed/lua/lazyvim/config/keymaps.lua#L5
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
--- fzf-lua bindings
--- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#commands
-local fzf = require("fzf-lua")
-
-vim.keymap.set("n", "<leader>p", fzf.files, { desc = "Files" })
-vim.keymap.set("n", "<leader>/", fzf.live_grep_glob, { desc = "Quick search" })
-vim.keymap.set("n", "<leader>fh", fzf.tags, { desc = "Find tags" })
-vim.keymap.set({ "n", "x" }, "zs", fzf.spell_suggest, { desc = "Spelling suguestions" })
+-- LSP
+vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<cr>", { desc = "Restart LSP Server" })
 
 -- Git
-vim.keymap.set("n", "<leader>gs", fzf.git_status, { desc = "Git Status" })
-vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Git blame" })
-vim.keymap.set("n", "<leader>gB", fzf.git_branches, { desc = "Git branches" })
+vim.keymap.set({ "n", "x" }, "<leader>gb", Snacks.picker.git_branches, { desc = "Git branches" })
 vim.keymap.set({ "n", "x" }, "<leader>go", function()
-  require("snacks").gitbrowse()
+  Snacks.gitbrowse()
 end, { desc = "Perm Link" })
-vim.keymap.set({ "n", "x" }, "<leader>gl", function()
-  require("snacks").gitbrowse({
+vim.keymap.set({ "n", "x" }, "<leader>gp", function()
+  ---@diagnostic disable-next-line: missing-fields
+  Snacks.gitbrowse({
     open = function(url)
       vim.fn.setreg("+", url)
     end,
   })
 end, { desc = "Perm Link (copy)" })
 vim.keymap.set("n", "<leader>gn", require("neogit").open, { desc = "Neogit" })
+
+-- Debugging
+vim.keymap.set("n", "<leader>dO", require("dap").step_out, { desc = "Step Out" })
+vim.keymap.set("n", "<leader>do", require("dap").step_over, { desc = "Step Over" })
+vim.keymap.set("n", "<leader>dg", require("dap").continue, { desc = "Go!" })
 
 -- Navigate between panes with arrow keys
 vim.keymap.set("n", "<LEFT>", "<C-w>h", { desc = "Go to Left Window", remap = true })
@@ -44,3 +42,6 @@ vim.keymap.set("n", "<leader>bP", "<Cmd>BufferLineTogglePin<CR>", { desc = "Togg
 vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- Utils
+vim.keymap.set({ "n", "x" }, "zs", Snacks.picker.spelling, { desc = "Spelling suguestions" })
